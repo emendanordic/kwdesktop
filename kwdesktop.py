27b481c21b454a672dbb9ab36f76f3dbb63a45f0
Jacob Larfors <jacob@larfors.com>
2016-11-07 11:34:22 +0200

    updated licence to MIT

--- kwdesktop.py
+++ kwdesktop.py
@@ -1,25 +1,25 @@
-# **************************************************************************************************
-#  Emenda Nordic AB.
+# MIT License
 #
-#  This script sets up a local Klocwork project using the command line tools.
-#  To use, make sure you have captured your build command with Klocwork's kwinject command
-#  to generate a valid build specification. The build specification is required to use this
-#  script.
+# Copyright (c) 2016 Emenda Nordic
 #
-#  Script also provides ability to create reports for Klocwork issues detected and also
-#  for calculated metrics
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#  $Author: Jacob Larfors
-#  $LastChangedBy: Jacob Larfors
-#  $LastChangedDate: 2016-10-25
-#  $Version: 1.0
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-# Disclaimer: Please note that this software or software component is released by Emenda Nordic AB
-# on a non-proprietary basis for commercial or non-commercial use with no warranty. Emenda Nordic AB
-# will not be liable for any damage or loss caused by the use of this software. Redistribution is
-# only allowed with prior consent.
-#
-# **************************************************************************************************
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 from kwcheck import Kwcheck
 
